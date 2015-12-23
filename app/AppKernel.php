7b6d1250cc763295fbb470fd5d41c3bd4cd3f2eb
jinky32 <jinky32@gmail.com>
2015-12-23 14:05:57 +0000

    added MopaBootstrapBundle, KnoMenuBundle and KnpPaginatorBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,9 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
