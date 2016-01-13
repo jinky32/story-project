e1a13e12e12f238187d28d40aea41e3afffdd494
jinky32 <jinky32@gmail.com>
2016-01-13 20:14:45 +0000

    commented out loads of bundles!

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,8 +36,8 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 //            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-            $bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
+           // $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            //$bundles[] = new Hautelook\AliceBundle\HautelookAliceBundle();
            
         }
 
